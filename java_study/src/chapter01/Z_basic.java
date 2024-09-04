935a4b4d9bcb9f0d1718b37c18a390ca3c290810
hiy0ung <hyejee98@gmail.com>
2024-09-04 17:27:12 +0900

    20240904

---
+++ java_study/src/chapter01/Z_basic.java
@@ -0,0 +1,33 @@
+package chapter01;
+
+public class Z_basic {
+
+	public static void main(String[] args) {
+		// 이클립스 단축키
+
+		// 자바 주석
+		// 단축키: ctrl + /
+
+		// 한 줄 주석: 슬래시(/) 2개
+		// 여러 줄 주석
+		/*
+		 * 슬래시 2개 사이에 별(*) 2개
+		 */
+
+		//* 줄 복사: ctrl + alt + 방향키(위/아래)
+		//* 줄 이동: alt + 방향키(위/아래)
+		//* 줄 정렬: ctrl + shift + f
+		// 줄 삭제: ctrl + d
+		
+		//* 파일 저장: ctrl + s
+		// 전체 파일 저장: ctrl + shift + s
+		
+		//* 파일 실행: ctrl + F11
+		
+		//* 이전 상황 돌아가기: ctrl + z
+		
+		// 안녕하세요
+
+	}
+
+}
